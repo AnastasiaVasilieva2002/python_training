e9d000ddfbbe6147dec0baf6eb87c582332ccd90
AnastasiaVasilieva2002 <107684915+AnastasiaVasilieva2002@users.noreply.github.com>
2022-06-29 19:59:41 +0400

    Выделен класс для управлениями сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -8,47 +8,41 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.vars = {}
+        self.session = SessionHelper(self)
+
 
     def open_home_page(self):
-        self.wd.get("http://localhost:81/addressbook/")
+        wd = self.wd
+        wd.get("http://localhost:81/addressbook/")
 
-    def login(self, username, password):
-        self.open_home_page()
-        self.wd.find_element(By.NAME, "user").click()
-        self.wd.find_element(By.NAME, "user").clear()
-        self.wd.find_element(By.NAME, "user").send_keys(username)
-        self.wd.find_element(By.NAME, "pass").click()
-        self.wd.find_element(By.NAME, "pass").clear()
-        self.wd.find_element(By.NAME, "pass").send_keys(password)
-        self.wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_groups_page(self):
+        wd = self.wd
         self.wd.find_element(By.LINK_TEXT,"groups").click()
 
     def create_group(self, group):
+        wd = self.wd
         self.open_groups_page()
         # init group creation
-        self.wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "new").click()
         # fill group form
-        self.wd.find_element(By.NAME, "group_name").click()
-        self.wd.find_element(By.NAME, "group_name").clear()
-        self.wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        self.wd.find_element(By.NAME, "group_header").click()
-        self.wd.find_element(By.NAME, "group_header").clear()
-        self.wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        self.wd.find_element(By.NAME, "group_footer").click()
-        self.wd.find_element(By.NAME, "group_footer").clear()
-        self.wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
-        self.wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
+        wd = self.wd
         self.wd.find_element(By.LINK_TEXT, "group page").click()
 
-    def logout(self):
-        self.wd.find_element(By.LINK_TEXT, "Logout").click()
-        time.sleep(1)
 
     def destroy(self):
         self.wd.quit()
